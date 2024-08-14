cc08e3085cd33c8dc613d0c0e7a982073d394177
matheusfranco01 <matheusfrancojf@gmail.com>
2024-08-14 14:34:11 -0300

    Alterações da pagina de premios

---
+++ Awards/script.js
@@ -0,0 +1,4 @@
+document.getElementById('botao').addEventListener('click', function() {
+    document.querySelector('.case1').classList.add('active');
+    document.querySelector('.case2').classList.add('active');
+});
